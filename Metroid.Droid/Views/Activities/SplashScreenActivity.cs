d11170cdd5ba4cc90118432a53ad749100a1b1a0
leglutz <frederic.jolidon@gmail.com>
2015-11-24 22:39:32 +0200

    Add Xamarin Insights

--- Metroid.Droid/Views/Activities/SplashScreenActivity.cs
+++ Metroid.Droid/Views/Activities/SplashScreenActivity.cs
@@ -1,14 +1,23 @@
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
 using Cirrious.MvvmCross.Droid.Views;
 
 namespace DiodeCompany.Metroid.Droid.Activities
 {
-    [Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/MyTheme.Splash", NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait)]
-    public class SplashScreenActivity : MvxSplashScreenActivity
-    {
-        public SplashScreenActivity() : base(Resource.Layout.activity_splash_screen)
-        {
-        }
-    }
+	[Activity (Label = "@string/app_name", MainLauncher = true, Theme = "@style/MyTheme.Splash", NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait)]
+	public class SplashScreenActivity : MvxSplashScreenActivity
+	{
+		public SplashScreenActivity () : base (Resource.Layout.activity_splash_screen)
+		{
+		}
+
+		protected override void OnCreate (Bundle bundle)
+		{
+            // Initialize Insights
+            Xamarin.Insights.Initialize("ab6566a0d14adf0748a3d916dca49e4cfe75fbd6", ApplicationContext);
+
+			base.OnCreate (bundle);
+		}
+	}
 }
