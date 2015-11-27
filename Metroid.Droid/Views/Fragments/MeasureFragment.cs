740079b62b5f9c8ac9424592d269f39589c463c4
leglutz <frederic.jolidon@gmail.com>
2015-11-27 15:48:45 +0200

    Set the seek bar max from code behind

--- Metroid.Droid/Views/Fragments/MeasureFragment.cs
+++ Metroid.Droid/Views/Fragments/MeasureFragment.cs
@@ -1,5 +1,6 @@
 using Android.OS;
 using Android.Views;
+using Android.Widget;
 using Cirrious.MvvmCross.Binding.Droid.BindingContext;
 using Cirrious.MvvmCross.Droid.Support.Fragging.Fragments;
 using DiodeCompany.Metroid.Core.ViewModels;
@@ -11,7 +12,15 @@ namespace DiodeCompany.Metroid.Droid.Views.Fragments
         public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var ignored = base.OnCreateView (inflater, container, savedInstanceState);
-            return this.BindingInflate (Resource.Layout.fragment_measure, null);
+            var view = this.BindingInflate (Resource.Layout.fragment_measure, null);
+
+            // Set the max value of the seek bar manually due to a binding problem with MvvmCross and Java
+            // http://stackoverflow.com/questions/28030458/mvvmcross-binding-android-eventhandler
+            // https://github.com/MvvmCross/MvvmCross/issues/884
+            var seekBar = view.FindViewById<SeekBar> (Resource.Id.seek_bar);
+            seekBar.Max = ViewModel.Measure.MaxTempo;
+
+            return view;
         }
     }
 }
