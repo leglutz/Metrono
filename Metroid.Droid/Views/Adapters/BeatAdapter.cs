7a9d64197f35ae454c8082e5d2d8990959d2803f
leglutz <frederic.jolidon@gmail.com>
2015-11-28 13:41:59 +0200

    Add values-fr and Adapters folder for BeatAdapter

---
+++ Metroid.Droid/Views/Adapters/BeatAdapter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Android.Content;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using DiodeCompany.Metroid.Core.Models;
+
+namespace DiodeCompany.Metroid.Droid.Views.Adapters
+{
+    public class BeatAdapter : BaseAdapter<Beat>
+    {
+        private readonly Context _context;
+        private readonly GridView _gridView;
+        private readonly ObservableCollection<Beat> _beatList;
+
+        public override int Count
+        {
+            get { return _beatList.Count; }
+        }
+
+        public override Beat this[int position]
+        {
+            get{ return _beatList [position]; }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public BeatAdapter(Context context, GridView gridView, ObservableCollection<Beat> beatList)
+        {
+            _context = context;
+            _gridView = gridView;
+            _beatList = beatList;
+            _beatList.CollectionChanged += (sender, e) => NotifyDataSetChanged ();
+        }
+
+        public override View GetView (int position, View convertView, ViewGroup parent)
+        {
+            var inflater = LayoutInflater.FromContext (_context);
+            var view = inflater.Inflate (Resource.Layout.template_metronome_beat_item, parent, false);
+
+            // Image resource
+            var beatImageView = view.FindViewById<ImageView> (Resource.Id.beat_image_view);
+            var imageResource = Resource.Drawable.quarter;
+            switch (this [position].Value)
+            {
+                case 1:
+                    imageResource = Resource.Drawable.whole;
+                    break;
+                case 2:
+                    imageResource = Resource.Drawable.half;
+                    break;
+                case 4:
+                    imageResource = Resource.Drawable.quarter;
+                    break;
+                case 8:
+                    imageResource = Resource.Drawable.eighth;
+                    break;
+                case 16:
+                    imageResource = Resource.Drawable.sixteenth;
+                    break;
+                case 32:
+                    imageResource = Resource.Drawable.thirty_second;
+                    break;
+            }
+            beatImageView.SetImageResource (imageResource);
+
+            // Beat number
+            var beatNumberTextView = view.FindViewById<TextView> (Resource.Id.beat_number_text_view);
+            beatNumberTextView.Text = this [position].Number.ToString();
+
+            // Layout size
+            const double numberOfColumns = 5.0;
+            const double numberOfRows = 4.0;
+
+            var width = _gridView.Width - _gridView.PaddingLeft - _gridView.PaddingRight;
+            var gridViewParent = (View)_gridView.Parent;
+            var height = gridViewParent.Height - gridViewParent.PaddingTop - gridViewParent.PaddingBottom;
+
+            var itemWidth = width / numberOfColumns - _gridView.HorizontalSpacing;
+            var itemHeight = height / numberOfRows - _gridView.VerticalSpacing;
+
+            var size = (int)Math.Min (itemWidth, itemHeight);
+
+            view.LayoutParameters = new GridView.LayoutParams (size, size);
+
+            // Initial values for card view
+            ((CardView)view).Alpha = 0.5f;
+            ((CardView)view).CardElevation = 0;
+
+            return view;
+        }
+    }
+}
+
--- Metroid.Droid/Views/Fragments/MetronomeFragment.cs
+++ Metroid.Droid/Views/Fragments/MetronomeFragment.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using Android.Animation;
-using Android.Content;
+﻿using Android.Animation;
 using Android.OS;
 using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
+using Cirrious.CrossCore;
 using Cirrious.CrossCore.Droid;
 using Cirrious.MvvmCross.Binding.Droid.BindingContext;
 using Cirrious.MvvmCross.Droid.Support.Fragging.Fragments;
@@ -13,31 +11,25 @@ using DiodeCompany.Metroid.Core.Models;
 using DiodeCompany.Metroid.Core.Services;
 using DiodeCompany.Metroid.Core.ViewModels;
 using DiodeCompany.Metroid.Droid.Views.Activities;
+using DiodeCompany.Metroid.Droid.Views.Adapters;
 
 namespace DiodeCompany.Metroid.Droid.Views.Fragments
 {
     public class MetronomeFragment : MvxFragment<MetronomeViewModel>, View.IOnTouchListener
     {
         private readonly Settings _settings;
-        private readonly Vibrator _vibrator;
 
         private ObjectAnimator _backgroundColorAnimator;
         private GridView _gridView;
+        private Vibrator _vibrator;
 
-        public MetronomeFragment(ISettingsService settingsService, IMvxAndroidGlobals globals)
+        public MetronomeFragment()
         {
-            _settings = settingsService.Settings;
-            _vibrator = (Vibrator)globals.ApplicationContext.GetSystemService(Android.Content.Context.VibratorService);
+            _settings = Mvx.Resolve<ISettingsService>().Settings;
+
+            RetainInstance = true;
         }
-
-        public override void OnViewModelSet ()
-        {
-            base.OnViewModelSet ();
-
-            ViewModel.Metronome.BeatStarted += OnBeatStarted;
-            ViewModel.Metronome.BeatFinished += OnBeatFinished;
-        }
-
+            
         public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var ignored = base.OnCreateView (inflater, container, savedInstanceState);
@@ -47,7 +39,10 @@ namespace DiodeCompany.Metroid.Droid.Views.Fragments
 
             // Beats layout background animation
             var beatsLayout = view.FindViewById<View>(Resource.Id.beats_layout);
-            _backgroundColorAnimator = ObjectAnimator.OfObject (beatsLayout, "backgroundColor", new ArgbEvaluator (), _settings.BlinkColor, 0);
+            _backgroundColorAnimator = ObjectAnimator.OfObject (beatsLayout, 
+                                                                "backgroundColor", 
+                                                                new ArgbEvaluator (), 
+                                                                _settings.BlinkColor, 0);
             beatsLayout.SetOnTouchListener (this);
 
             // GridView
@@ -57,11 +52,31 @@ namespace DiodeCompany.Metroid.Droid.Views.Fragments
             _gridView.SetOnTouchListener (this);
 
             // Measure fragment
+            var measureFragment = new MeasureFragment()  { ViewModel = ViewModel.MeasureViewModel };
             ChildFragmentManager.BeginTransaction ()
-                .Replace (Resource.Id.measure_frame, new MeasureFragment { ViewModel = ViewModel.MeasureViewModel })
+                .Replace (Resource.Id.measure_frame, measureFragment)
                 .Commit ();
+
+            // Vibrator
+            _vibrator = (Vibrator) Activity.GetSystemService(Android.Content.Context.VibratorService);
             
             return view;
+        }
+
+        public override void OnStart ()
+        {
+            base.OnStart ();
+
+            ViewModel.Metronome.BeatStarted += OnBeatStarted;
+            ViewModel.Metronome.BeatFinished += OnBeatFinished;
+        }
+
+        public override void OnStop ()
+        {
+            ViewModel.Metronome.BeatStarted -= OnBeatStarted;
+            ViewModel.Metronome.BeatFinished -= OnBeatFinished;
+
+            base.OnStop ();
         }
 
         public bool OnTouch (View v, MotionEvent e)
@@ -123,93 +138,6 @@ namespace DiodeCompany.Metroid.Droid.Views.Fragments
                     ((CardView)beatView).CardElevation = 0;
                 }
             });
-        }
-
-        class BeatAdapter : BaseAdapter<Beat>
-        {
-            private readonly Context _context;
-            private readonly GridView _gridView;
-            private readonly ObservableCollection<Beat> _beatList;
-
-            public override int Count
-            {
-                get { return _beatList.Count; }
-            }
-
-            public override Beat this[int position]
-            {
-                get{ return _beatList [position]; }
-            }
-
-            public override long GetItemId(int position)
-            {
-                return position;
-            }
-
-            public BeatAdapter(Context context, GridView gridView, ObservableCollection<Beat> beatList)
-            {
-                _context = context;
-                _gridView = gridView;
-                _beatList = beatList;
-                _beatList.CollectionChanged += (sender, e) => NotifyDataSetChanged ();
-            }
-
-            public override View GetView (int position, View convertView, ViewGroup parent)
-            {
-                var inflater = LayoutInflater.FromContext (_context);
-                var view = inflater.Inflate (Resource.Layout.template_metronome_beat_item, parent, false);
-
-                // Image resource
-                var beatImageView = view.FindViewById<ImageView> (Resource.Id.beat_image_view);
-                var imageResource = Resource.Drawable.quarter;
-                switch (this [position].Value)
-                {
-                    case 1:
-                        imageResource = Resource.Drawable.whole;
-                        break;
-                    case 2:
-                        imageResource = Resource.Drawable.half;
-                        break;
-                    case 4:
-                        imageResource = Resource.Drawable.quarter;
-                        break;
-                    case 8:
-                        imageResource = Resource.Drawable.eighth;
-                        break;
-                    case 16:
-                        imageResource = Resource.Drawable.sixteenth;
-                        break;
-                    case 32:
-                        imageResource = Resource.Drawable.thirty_second;
-                        break;
-                }
-                beatImageView.SetImageResource (imageResource);
-
-                // Beat number
-                var beatNumberTextView = view.FindViewById<TextView> (Resource.Id.beat_number_text_view);
-                beatNumberTextView.Text = this [position].Number.ToString();
-
-                // Layout size
-                const double numberOfColumns = 5.0;
-                const double numberOfRows = 4.0;
-
-                var width = _gridView.Width - _gridView.PaddingLeft - _gridView.PaddingRight;
-                var gridViewParent = (View)_gridView.Parent;
-                var height = gridViewParent.Height - gridViewParent.PaddingTop - gridViewParent.PaddingBottom;
-
-                var itemWidth = width / numberOfColumns - _gridView.HorizontalSpacing;
-                var itemHeight = height / numberOfRows - _gridView.VerticalSpacing;
-
-                var size = (int)Math.Min (itemWidth, itemHeight);
-
-                view.LayoutParameters = new GridView.LayoutParams (size, size);
-
-                // Initial values for card view
-                ((CardView)view).Alpha = 0.5f;
-                ((CardView)view).CardElevation = 0;
-
-                return view;
-            }
         }
     }
 }
